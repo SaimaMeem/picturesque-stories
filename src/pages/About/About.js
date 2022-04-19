e2c722ab8e900b2cb09036129e5a5306a66f4b5e
SaimaMeem <saimasultana1320@gmail.com>
2022-04-20 02:37:03 +0600

    minor changes done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import '../About/About.css'
 const About = () => {
     return (
-        <Container className='my-5 mx-auto pb-5 about w-50'>
+        <Container className='my-5 mx-auto pb-5 about'>
           <h2>About Me</h2>
           <p className='about-me'>My name is Saima Sultana. I have completed my Bsc in Computer Science and Engineering in 2021. Currently, I am working as a ERPNext Fullstack Engineer in Altersense Limited. 
           <br/>
